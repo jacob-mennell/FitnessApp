30aaece3e5310d2faa4e3ccd47b5f3a148acfaa9
Jacob Mennell <jacob.mennell@canopius.com>
2024-02-18 14:17:34 +0000

    duck db test

--- test/test_duck_db.py
+++ test/test_duck_db.py
@@ -1,58 +1,32 @@
+import pytest
 import duckdb
 
 
 def check_table_existence(database_path, table_name):
-    con = None
-    try:
-        # Connect to the DuckDB database
-        con = duckdb.connect(database_path)
+    con = duckdb.connect(database_path)
+    query = f"SELECT 1 FROM information_schema.tables WHERE table_name = {repr(table_name)} LIMIT 1"
+    result = con.execute(query).fetchall()
+    con.close()
+    return bool(result)
 
-        # Check if the table exists
-        query = f"SELECT 1 FROM information_schema.tables WHERE table_name = {repr(table_name)} LIMIT 1"
-        result = con.execute(query).fetchall()
 
-        if result:
-            print(f"Table '{table_name}' exists in the database.")
-        else:
-            print(f"Table '{table_name}' does not exist in the database.")
-    except Exception as e:
-        print(f"Error: {str(e)}")
-    finally:
-        # Close the connection if it's not None
-        if con is not None:
-            con.close()
+def test_check_table_existence():
+    database_path = "database/fit.db"
+    table_name = "historic_exercises"
+    assert check_table_existence(database_path, table_name) == True
 
 
 def list_tables(database_path):
-    con = None
-    try:
-        # Connect to the DuckDB database
-        con = duckdb.connect(database_path)
-
-        # Query to list all tables
-        query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
-        result = con.execute(query).fetchdf()
-
-        if not result.empty:
-            print("Tables in the database:")
-            for table in result["table_name"]:
-                print(table)
-        else:
-            print("No tables found in the database.")
-    except Exception as e:
-        print(f"Error: {str(e)}")
-    finally:
-        # Close the connection if it's not None
-        if con is not None:
-            con.close()
+    con = duckdb.connect(database_path)
+    query = (
+        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
+    )
+    result = con.execute(query).fetchdf()
+    con.close()
+    return result["table_name"].tolist()
 
 
-# Specify the database path and table name
-database_path = "test.db"
-table_name = "historic_exercises"
-
-# Call the function to check table existence
-# check_table_existence(database_path, table_name)
-
-# Comment out the function to list tables
-list_tables(database_path)
+def test_list_tables():
+    database_path = "database/fit.db"
+    expected_tables = ["historic_exercises"]
+    assert list_tables(database_path) == expected_tables
